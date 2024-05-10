f887bd7dd88759521336fa5dafa18b9194875c9d
coco <43858608+Amandal4012@users.noreply.github.com>
2024-05-10 19:53:25 -0400

    implement new better movement system script

--- Second Hero Born/Assets/Scripts/PlayerBehavior.cs
+++ Second Hero Born/Assets/Scripts/PlayerBehavior.cs
@@ -4,32 +4,39 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // 1
+    
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
-
-    // 2
     private float _vInput;
     private float _hInput;
+
+    private Rigidbody _rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 3 
+      
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
-
-        // 4
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
 
-        // 5 
+        /*
         this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
-
-        // 6 
         this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
+        */
+    }
+
+     void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * _hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
